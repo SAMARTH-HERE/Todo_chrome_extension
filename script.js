54a00de54593fc59001625b403d2daaae52441a1
samarth kumar <samarthgupta316@gmail.com>
2024-10-06 21:39:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const input = document.getElementById("input-box");
+const list = document.getElementById("list");
+const addButton = document.getElementById("add-button");
+
+// Set up event listener for the Add button
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+    if (input.value === '') {
+        alert("You need to write something in the input box");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = input.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    input.value = ""; // Clear the input field
+    saveData();
+}
+
+list.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask() {
+    let data = localStorage.getItem("data");
+    if (data) {
+        list.innerHTML = data;
+    }
+}
+
+showTask();
